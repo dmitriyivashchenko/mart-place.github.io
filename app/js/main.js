e605d27e974638d8ac4c84281ee5d3f0d4bfe8b2
dmitriyivashchenko <markfederal4@gmail.com>
2020-05-24 16:09:51 +0300

    added Weekly Products section

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    $('.rate-stars').rateYo({
+        rating: 4.6,
+        starWidth: "15px",
+        ratedFill: "#ffc000",
+        normalFill: '#c7c8c8',
+        readOnly: true
+    });
+
+    $('.slick__single-item').slick({
+        appendArrows: $('.slider-buttons'),
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next')
+    });
+ 
+})
--- gulpfile.js
+++ gulpfile.js
@@ -24,28 +24,21 @@ gulp.task('script', function() {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app/js'))
 })
 
-// gulp.task('js-compress', function() {
-//     return gulp.src(
-//         'app/js/main.js',
-//         { allowEmpty: true }
-//     )
-//     .pipe(concat('main.min.js'))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('app/js'))
-// })
 
 gulp.task('style', function() {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css',
-        'node_modules/normalize.css/normalize.css'
+        'node_modules/normalize.css/normalize.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
